7ba1f7ffc378aaa05ddf569f46f83b8b7ef68704
ShumzZzZz <shumin.zheng@sayweee.com>
2025-04-14 21:55:28 -0700

    add more steps to follow

--- tofu/scripts/feast_setup/get_yaml_files.sh
+++
@@ -1,3 +0,0 @@
-curl -sSLO https://raw.githubusercontent.com/ShumzZzZz/Devour/refs/heads/main/tofu/scripts/feast_setup/feast.yaml
-curl -sSLO https://raw.githubusercontent.com/ShumzZzZz/Devour/refs/heads/main/tofu/scripts/feast_setup/feast_operator_install.yaml
-curl -sSLO https://raw.githubusercontent.com/ShumzZzZz/Devour/refs/heads/main/tofu/scripts/feast_setup/prerequisite_setup.yaml
