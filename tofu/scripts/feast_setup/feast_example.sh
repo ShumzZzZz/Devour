af51e376c1c4f25e300e4019c6441c6a395ba6f4
ShumzZzZz <shumin.zheng@sayweee.com>
2025-04-14 22:17:17 -0700

    modify ephemeral storage request & limits for the deployments

--- tofu/scripts/feast_setup/feast_example.sh
+++ tofu/scripts/feast_setup/feast_example.sh
@@ -6,18 +6,18 @@ kubectl config set-context --current --namespace feast
 kubectl label nodes <nodes> dedicated=featurestore
 kubectl taint nodes <nodes> dedicated=featurestore:NoExecute
 
-curl -sSLO https://raw.githubusercontent.com/ShumzZzZz/Devour/refs/heads/main/tofu/scripts/feast_setup/prerequisite_setup.yaml
+curl -sSLO https://raw.githubusercontent.com/ShumzZzZz/Devour/refs/heads/provision-with-opentofu/tofu/scripts/feast_setup/prerequisite_setup.yaml
 kubectl apply -f prerequisite_setup.yaml
 
 kubectl wait --for=condition=available --timeout=5m deployment/redis
 kubectl wait --for=condition=available --timeout=5m deployment/postgres
 
-curl -sSLO https://raw.githubusercontent.com/ShumzZzZz/Devour/refs/heads/main/tofu/scripts/feast_setup/feast_operator_install.yaml
+curl -sSLO https://raw.githubusercontent.com/ShumzZzZz/Devour/refs/heads/provision-with-opentofu/tofu/scripts/feast_setup/feast_operator_install.yaml
 kubectl apply -f feast_operator_install.yaml
 
 kubectl wait --for=condition=available --timeout=5m deployment/feast-operator-controller-manager -n feast
 
-curl -sSLO https://raw.githubusercontent.com/ShumzZzZz/Devour/refs/heads/main/tofu/scripts/feast_setup/feast.yaml
+curl -sSLO https://raw.githubusercontent.com/ShumzZzZz/Devour/refs/heads/provision-with-opentofu/tofu/scripts/feast_setup/feast.yaml
 kubectl apply -f feast.yaml
 
 kubectl wait --for=condition=available --timeout=8m deployment/feast-example
