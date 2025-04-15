fea0c8801e0d12c535fb45217d38cb9221bad0ca
ShumzZzZz <shumin.zheng@sayweee.com>
2025-04-14 17:01:23 -0700

    initial commit, moving out of a non-version-controlled directory

---
+++ tofu/scripts/feast_setup/get_yaml_files.sh
@@ -0,0 +1,3 @@
+curl -sSLO https://raw.githubusercontent.com/ShumzZzZz/Devour/refs/heads/main/tofu/scripts/feast_setup/feast.yaml
+curl -sSLO https://raw.githubusercontent.com/ShumzZzZz/Devour/refs/heads/main/tofu/scripts/feast_setup/feast_operator_install.yaml
+curl -sSLO https://raw.githubusercontent.com/ShumzZzZz/Devour/refs/heads/main/tofu/scripts/feast_setup/prerequisite_setup.yaml
