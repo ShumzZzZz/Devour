9c8d3a61d91bbb2506d0fee7d48c3c263d24ddf2
ShumzZzZz <shumin.zheng@sayweee.com>
2025-04-18 15:48:51 -0700

    update init script

--- tofu/scripts/feast_setup/feast_example.sh
+++ tofu/scripts/feast_setup/feast_example.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
+alias k='kubectl'
+alias kd='kubectl describe'
 
-kubectl create ns feast
-kubectl config set-context --current --namespace feast
+k create ns feast
+k config set-context --current --namespace feast
 
-#kubectl label nodes ip-10-10-1-140 dedicated=featurestore
-#kubectl label nodes ip-10-10-1-175 dedicated=featurestore
-#kubectl label nodes ip-10-10-1-226 dedicated=featurestore
-#kubectl taint nodes ip-10-10-1-140 dedicated=featurestore:NoExecute
-#kubectl taint nodes ip-10-10-1-175 dedicated=featurestore:NoExecute
-#kubectl taint nodes ip-10-10-1-226 dedicated=featurestore:NoExecute
-
-kubectl label nodes <nodes> dedicated=featurestore
-kubectl taint nodes <nodes> dedicated=featurestore:NoExecute
+k get nodes --no-headers | awk '$3 != "control-plane" {print $1}' | while read node; do
+  k label node "$node" dedicated=featurestore --overwrite
+  k taint node "$node" dedicated=featurestore:NoExecute --overwrite
+done
 
 
 curl -sSLO https://raw.githubusercontent.com/ShumzZzZz/Devour/refs/heads/provision-with-opentofu/tofu/scripts/feast_setup/feast_scheduling_pref.yaml
@@ -19,29 +16,29 @@ helm install kyverno kyverno/kyverno -n kyverno --create-namespace \
   --values feast_scheduling_pref.yaml
 
 curl -sSLO https://raw.githubusercontent.com/ShumzZzZz/Devour/refs/heads/provision-with-opentofu/tofu/scripts/feast_setup/kyverno_cluster_policy.yaml
-kubectl apply -f kyverno_cluster_policy.yaml
+k apply -f kyverno_cluster_policy.yaml
 
 curl -sSLO https://raw.githubusercontent.com/ShumzZzZz/Devour/refs/heads/provision-with-opentofu/tofu/scripts/feast_setup/prerequisite_setup.yaml
-kubectl apply -f prerequisite_setup.yaml
+k apply -f prerequisite_setup.yaml
 
-kubectl wait --for=condition=available --timeout=5m deployment/redis-feast
-kubectl wait --for=condition=available --timeout=5m deployment/postgres-feast
+k wait --for=condition=available --timeout=5m deployment/redis-feast
+k wait --for=condition=available --timeout=5m deployment/postgres-feast
 
 curl -sSLO https://raw.githubusercontent.com/ShumzZzZz/Devour/refs/heads/provision-with-opentofu/tofu/scripts/feast_setup/feast_operator_install.yaml
-kubectl apply -f feast_operator_install.yaml
+k apply -f feast_operator_install.yaml
 
-kubectl wait --for=condition=available --timeout=5m deployment/feast-operator-controller-manager -n feast
+k wait --for=condition=available --timeout=5m deployment/feast-operator-controller-manager -n feast
 
 curl -sSLO https://raw.githubusercontent.com/ShumzZzZz/Devour/refs/heads/provision-with-opentofu/tofu/scripts/feast_setup/feast.yaml
-kubectl apply -f feast.yaml
+k apply -f feast.yaml
 
-kubectl wait --for=condition=available --timeout=8m deployment/feast-example
+k wait --for=condition=available --timeout=8m deployment/feast-example
 
-kubectl exec deployment/postgres-feast -- psql -h localhost -U feastuser feastdb -c '\dt'
-kubectl exec deployment/feast-example -itc online -- feast version
+k exec deployment/postgres-feast -- psql -h localhost -U feastuser feastdb -c '\dt'
+k exec deployment/feast-example -itc online -- feast version
 
 
 
-kubectl delete -f feast.yaml
-kubectl delete -f feast_operator_install.yaml
-kubectl delete -f prerequisite_setup.yaml
+k delete -f feast.yaml
+k delete -f feast_operator_install.yaml
+k delete -f prerequisite_setup.yaml
