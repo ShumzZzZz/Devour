d74e98a44ad5a0eec9a641eedee6670a36a69b4d
ShumzZzZz <shumin.zheng@icloud.com>
2025-05-06 15:38:39 -0700

    set up remote client connection

--- tofu/scripts/feast_setup/feast_interaction_python_sdk.py
+++ tofu/scripts/feast_setup/feast_interaction_python_sdk.py
@@ -2,5 +2,17 @@ from feast import FeatureStore
 
 fs = FeatureStore(fs_yaml_file='./feature_store_remote_client.yaml')
 
-print(fs.registry.list_entities(project="credit_scoring_local"))
+# for p in fs.registry.list_projects():
+# 	print(p)
 
+res = fs.get_online_features(
+	features=[
+		"zipcode_features:state",
+		"zipcode_features:population", ],
+	entity_rows=[
+		{"zipcode": 7675},
+	],
+
+).to_dict()
+
+print(res)
