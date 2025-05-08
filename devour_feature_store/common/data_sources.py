c48eb00a5f7b1bcd19aa634f7136d08bc2179183
ShumzZzZz <shumin.zheng@icloud.com>
2025-05-08 12:00:46 -0700

    update filesource spec

--- devour_feature_store/common/data_sources.py
+++ devour_feature_store/common/data_sources.py
@@ -3,13 +3,13 @@ from feast import (
     PushSource,
     RequestSource,
 )
-
+from feast.infra.offline_stores.contrib.spark_offline_store.spark_source import SparkSource
 from feast import ValueType
 from feast.data_format import ParquetFormat
 
 product_general_score_source = FileSource(
     name="product_general_score_source",
-    path="/Users/shuminzheng/PycharmProjects/devour/devour_feature_store/data/product_features/",
+    path="data/product_features/product_general_score.parquet",
     file_format=ParquetFormat(),
     timestamp_field="event_timestamp",
 )
