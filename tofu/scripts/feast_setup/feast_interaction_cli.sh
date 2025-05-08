ad6f6d74fccf77db1b0ba8d0569ce8e2a97cf6ac
ShumzZzZz <shumin.zheng@icloud.com>
2025-05-08 15:12:16 -0700

    update test script

--- tofu/scripts/feast_setup/feast_interaction_cli.sh
+++ tofu/scripts/feast_setup/feast_interaction_cli.sh
@@ -2,7 +2,12 @@
 
 curl "http://localhost:8002/get-online-features" \
 	--json '{
-		"features": ["zipcode_features:state", "zipcode_features:population"],
-		"entities": {"zipcode": [7675, 94538]}
+		"features": ["product_general_score:general_score", "product_general_score:event_timestamp"],
+		"entities": {"product_id": [0]}
 	}' \
 	| jq
+
+#	--json '{
+#		"features": ["zipcode_features:state", "zipcode_features:population"],
+#		"entities": {"zipcode": [7675, 94538]}
+#	}' \
--- tofu/scripts/feast_setup/feast_interaction_python_sdk.py
+++ tofu/scripts/feast_setup/feast_interaction_python_sdk.py
@@ -4,13 +4,16 @@ fs = FeatureStore(fs_yaml_file='./feature_store_remote_client.yaml')
 
 # for p in fs.registry.list_projects():
 # 	print(p)
+#
+# for fv in fs.list_feature_views():
+# 	print(fv)
 
 res = fs.get_online_features(
 	features=[
-		"zipcode_features:state",
-		"zipcode_features:population", ],
+		"product_general_score:general_score",
+		"product_general_score:event_timestamp", ],
 	entity_rows=[
-		{"zipcode": 7675},
+		{"product_id": 1005},
 	],
 
 ).to_dict()
