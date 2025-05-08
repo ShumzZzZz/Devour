8a4bdbcb2682c442b740e52ca3664e5d5694dadd
ShumzZzZz <shumin.zheng@icloud.com>
2025-05-07 18:15:55 -0700

    add feast official example

--- devour_feature_store/common/data_sources.py
+++ devour_feature_store/common/data_sources.py
@@ -5,9 +5,11 @@ from feast import (
 )
 
 from feast import ValueType
+from feast.data_format import ParquetFormat
 
 product_general_score_source = FileSource(
     name="product_general_score_source",
-    path="/Users/shuminzheng/PycharmProjects/devour/devour_feature_store/data/product_features",
+    path="/Users/shuminzheng/PycharmProjects/devour/devour_feature_store/data/product_features/",
+    file_format=ParquetFormat(),
     timestamp_field="event_timestamp",
 )
