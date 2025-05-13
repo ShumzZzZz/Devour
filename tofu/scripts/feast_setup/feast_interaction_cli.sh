ecbdd48f45ea133b238ca19d3ae92e7c1df85778
ShumzZzZz <shumin.zheng@icloud.com>
2025-05-13 14:12:21 -0700

    add more example and update init shell script

--- tofu/scripts/feast_setup/feast_interaction_cli.sh
+++ tofu/scripts/feast_setup/feast_interaction_cli.sh
@@ -7,6 +7,13 @@ curl "http://localhost:8002/get-online-features" \
 	}' \
 	| jq
 
+
+#	--json '{
+#		"features": ["product_general_score:general_score", "product_general_score:event_timestamp"],
+#		"entities": {"product_id": [0]}
+#	}' \
+#	| jq
+
 #	--json '{
 #		"features": ["zipcode_features:state", "zipcode_features:population"],
 #		"entities": {"zipcode": [7675, 94538]}
