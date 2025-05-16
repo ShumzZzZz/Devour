b5427ccdeb7316ab61e3eedf576dee70c5ee3b45
ShumzZzZz <shumin.zheng@icloud.com>
2025-05-16 16:36:23 -0700

    update workflow to ingest large amount of data

--- devour_feature_store/workflow_test.py
+++ devour_feature_store/workflow_test.py
@@ -1,23 +1,42 @@
 from feast import FeatureStore
 import pandas as pd
+import numpy as np
+from datetime import datetime
 from feast.data_source import PushMode
 
 
 def run_test():
 	fs = FeatureStore()
 
-	data = {
-		"product_id": [9],
-		"general_score": [0.009009009],
-		"event_timestamp": ["2025-05-08 14:24:00.000000"]
-	}
-	feature_df = pd.DataFrame(data)
+	n = 1_000_000
 
-	feature_df["event_timestamp"] = pd.to_datetime(feature_df["event_timestamp"])
+	# Sequential product IDs from 900001 to 920000
+	product_ids = np.arange(900_001, 900_001 + n)
+
+	# Generate random general scores between 0 and 1
+	general_scores = np.random.random(size=n)
+
+	# Use today's timestamp for all events
+	event_timestamp = datetime.now()
+
+	# Build the DataFrame
+	df = pd.DataFrame({
+		'product_id': product_ids,
+		'general_score': general_scores,
+		# 'general_score_1': general_scores,
+		# 'general_score_2': general_scores,
+		# 'general_score_3': general_scores,
+		# 'general_score_4': general_scores,
+		# 'general_score_5': general_scores,
+		# 'general_score_6': general_scores,
+		# 'general_score_7': general_scores,
+		# 'general_score_8': general_scores,
+		'event_timestamp': [event_timestamp] * n
+	})
 
 	fs.push(
 		push_source_name="general_score_push_source",
-		df=feature_df,
+		df=df,
 		to=PushMode.ONLINE  # _AND_OFFLINE
 	)
 
